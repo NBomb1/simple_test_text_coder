24661283ceab4830ce3f4c6fb858fb2152bb7704
ArT <65818228+NBomb1@users.noreply.github.com>
2021-07-28 18:46:24 +0900

    Update updater.py

--- updater.py
+++ updater.py
@@ -1,17 +1,23 @@
 from urllib3 import PoolManager, request
 from urllib import request as request1, response
-print(True)
-print('Start downloading\n')
-with request1.urlopen('https://raw.githubusercontent.com/NBomb1/simple_test_text_coder/main/coder.py') as response, open('coder.py', 'w') as coder:
+print('Start downloading.\n')
+with request1.urlopen('https://raw.githubusercontent.com/NBomb1/simple_test_text_coder/main/coder.py') as response, open('coder.txt', 'w') as coder:
     counter = 0
     text1 = response.readlines()
+    print(text1)
     while True:
         try:
             text = str(text1[counter])
         except IndexError:
             break
-        text = text.replace(text[0], '').replace(text[1], '').replace(text[len(text) - 1], '').replace(text[len(text) - 2], '').replace(text[len(text) - 3], '')
-        coder.write(f'{text}\n')
+        text = text.replace(text[0], '', 1).replace(text[1], '', 1)
+        if len(text.replace(r'\n"', '')) != len(text):
+             text = text.rstrip(r'\n"')
+        else:
+             text = text.replace(r"\n'", '')
+        print(text)
+        updater.write(f'{text}\n')
         counter += 1
+        updater.close()
+        print('Download has ended.')
 print('Download has ended.')
-print(False)
