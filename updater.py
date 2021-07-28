86883a2bf3f39e86bc398c79a3a521766a336d11
ArT <65818228+NBomb1@users.noreply.github.com>
2021-07-28 17:21:05 +0900

    Update updater.py

--- updater.py
+++ updater.py
@@ -1,5 +1,6 @@
 from urllib3 import PoolManager, request
 from urllib import request as request1, response
+print(True)
 print('Start downloading\n')
 with request1.urlopen('https://raw.githubusercontent.com/NBomb1/simple_test_text_coder/main/coder.py') as response, open('coder.py', 'w') as coder:
     counter = 0
@@ -13,3 +14,4 @@ with request1.urlopen('https://raw.githubusercontent.com/NBomb1/simple_test_text
         coder.write(f'{text}\n')
         counter += 1
 print('Download has ended.')
+print(False)
