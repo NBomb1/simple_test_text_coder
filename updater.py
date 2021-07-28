501dd90cad435fb242af44e43df3b7f9dac88f87
ArT <65818228+NBomb1@users.noreply.github.com>
2021-07-28 14:13:50 +0900

    Update updater.py

--- updater.py
+++ updater.py
@@ -1,3 +1,4 @@
+from urllib3 import PoolManager, request
 from urllib import request as request1, response
 print('Start downloading\n')
 with request1.urlopen('https://raw.githubusercontent.com/NBomb1/simple_test_text_coder/main/coder.py') as response, open('coder.txt', 'w') as coder:
