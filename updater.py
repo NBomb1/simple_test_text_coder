06e020b0c0e4c248a7e8f786c9fcee87b9e0dc53
ArT <65818228+NBomb1@users.noreply.github.com>
2021-07-28 13:51:33 +0900

    Update updater.py

--- updater.py
+++ updater.py
@@ -1,7 +1,13 @@
-print(112)
-
-
-print(212)
-
-print(1)
-print(2)
+print('Start downloading\n')
+with request1.urlopen('https://raw.githubusercontent.com/NBomb1/simple_test_text_coder/main/coder.py') as response, open('coder.txt', 'w') as coder:
+    counter = 0
+    text1 = response.readlines()
+    while True:
+        try:
+            text = str(text1[counter])
+        except IndexError:
+            break
+        text = text.replace(text[0], '').rstrip("'").lstrip("'").replace(text[len(text) - 3], '').rstrip('n')
+        updater.write(f'{text}\n')
+        counter += 1
+print('Download has ended.')
